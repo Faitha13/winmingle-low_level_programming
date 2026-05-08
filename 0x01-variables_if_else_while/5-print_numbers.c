6da5822d3001988ee680aaa2512bd432084f3f7a
Faitha13 <nwezefaithakwugo4christ@gmail.com>
2026-05-08 13:08:33 +0100

    A script 5hat prints all the numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+* Author: Nweze Faith Akwugo 
+* Program: WinMingle Community C Training
+* Description:  Add program that prints all the numbers in base 10
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	char x = '0';
+	while(x <= '9'){
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+return(0);	
+}
+
