44b6b7796ca0b9492a1f9bb6a2119f9c02b803a3
Faitha13 <nwezefaithakwugo4christ@gmail.com>
2026-05-09 14:38:59 +0100

    A script that prints all the numbers in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+/**
+* Author: Nweze Faith Akwugo 
+* Program: WinMingle Community C Training
+* Description:  Add program that prints all the numbers in base 10
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 16; i++)
+    {
+        printf("%x", i);
+    }
+
+    printf("\n");
+return(0);	
+}
+
