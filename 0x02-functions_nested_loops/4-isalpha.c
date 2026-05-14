47c595bd96661aabb7893176694093af1cfb0b2f
Faitha13 <nwezefaithakwugo4christ@gmail.com>
2026-05-15 00:23:30 +0100

    A script that check for alphetic characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks for alphabetic character
+ * @c: character to check
+ *
+ * Return: 1 if an al0habet, 0 otherwise
+ */
+
+int _isalpha(int c){
+	if((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z')){
+		return (1);
+	}
+	else{
+		return(0);
+	}
+}
