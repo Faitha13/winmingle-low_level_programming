4a7cc5bff0c03b818f7efa597f91b894928ca0de
Faitha13 <nwezefaithakwugo4christ@gmail.com>
2026-07-20 15:29:40 +0100

    a program that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+/***
+ *
+ * this program prints the number of input gotten
+ * apart from the program name
+ *
+ ***/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+	if(argc != 1)
+	printf("%d\n", argc);
+		return 0;
+
+}
