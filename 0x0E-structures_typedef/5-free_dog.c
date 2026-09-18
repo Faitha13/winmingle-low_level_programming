ec9fa9f1f8900cefe17a891ebbca5af3f7a0e21f
Faitha13 <nwezefaithakwugo4christ@gmail.com>
2026-09-18 09:53:27 +0100

    A file that Manage dog structures and memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+/***
+ * Manage dog structures and memory
+ ***/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+void free_dog(dog_t *d){
+	if(d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+
+}
